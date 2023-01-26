643667ba1156ed380c02a136d9caf713961f624c
Mladen <mladen.cvetanovic@outlook.com>
2023-01-26 16:12:49 +0100

    Navbar ispravak

--- src/App.js
+++ src/App.js
@@ -4,32 +4,36 @@ import LoginComponent from './components/login/login.component';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/NavBar/navbar';
 import Pocetna from './components/NavBar/links/Pocetna';
-
+import Informacije from './components/NavBar/links/Informacije'
 
 
 function App() {
 
-  return (<>
-    <div className='header'>
+  return (
+  
       <Router>
-      <Navbar />
-      <Switch>
-        <Route path="links/pocetna">
-          <Pocetna /> 
-        </Route>
-        <Route path='login.component'>
-          <LoginComponent />
-        </Route>
-      </Switch>
-      </Router>
-    </div>
-   <div className='main'>
-    
-    </div>
-    <div className='footer'>
-    
-    </div>
-  </>
+        <div className="App">
+          <Navbar />
+          <div className="content">
+            <Switch>
+              
+              <Route path ="/pocetna">
+                  <Pocetna />
+              </Route>
+              <Route path="/informacije">
+                <Informacije />
+              </Route>
+              <Route path="/login.component">
+                <LoginComponent />
+              </Route>
+
+            </Switch>
+
+          
+          </div>
+        </div>
+        </Router>
+  
   );
 }
 
