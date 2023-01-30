6ef00cb80b52c03c6c8968b8f266bca329ae92a9
Lazar\Lazar <laki098@gmail.com>
2023-01-30 16:17:02 +0100

    dodavanje validacije za registraciju

--- src/components/registration/registration.component.js
+++ src/components/registration/registration.component.js
@@ -1,6 +1,8 @@
 import { useRef, useState } from "react";
+import { ToastContainer } from "react-toastify";
 import RegistrationLogic from "./registration.logic";
 import classes from "./registration.module.css"
+import 'react-toastify/dist/ReactToastify.css';
 
 const RegistrationComponent = () => {
   let [formInputsValid, setFormInputsValid] = useState({name: true, prezime: true, email: true, korisnickoIme: true, lozinka: true, brojTelefona: true})
@@ -27,6 +29,7 @@ const RegistrationComponent = () => {
     registrationLogic.registracija()
   }
   return (
+  <>
     <form onSubmit={confirmeHandler} className={classes.form} >
       
       <div className={`${classes.control} ${formInputsValid.name ? "" : classes.invalid}`}>
@@ -47,6 +50,7 @@ const RegistrationComponent = () => {
           ref={prezimeInputRef}
           onChange={registrationLogic.changeHandler}
         />
+        {!formInputsValid.prezime && <p>Unesite prezime</p>} 
       </div>
       <div className={`${classes.control} ${formInputsValid.korisnickoIme ? "" : classes.invalid}`}>
         <label>Koriscko ime:</label>
@@ -56,6 +60,7 @@ const RegistrationComponent = () => {
           ref={korisnickoImeInputRef}
           onChange={registrationLogic.changeHandler}
         ></input>
+        {!formInputsValid.korisnickoIme && <p>Unesite korisničko ime</p>} 
       </div>
       <div className={`${classes.control} ${formInputsValid.lozinka ? "" : classes.invalid}`}>
         <label>Lozinka:</label>
@@ -65,6 +70,7 @@ const RegistrationComponent = () => {
           ref={lozinkaInputRef}
           onChange={registrationLogic.changeHandler}
         ></input>
+        {!formInputsValid.lozinka && <p>Unesite lozinku</p>} 
       </div>
       <div className={`${classes.control} ${formInputsValid.brojTelefona ? "" : classes.invalid}`}>
         <label>Broj telefona:</label>
@@ -74,6 +80,7 @@ const RegistrationComponent = () => {
           ref={brojTelefonaInputRef}
           onChange={registrationLogic.changeHandler}
         ></input>
+        {!formInputsValid.brojTelefona && <p>Unesite broj telefona</p>} 
       </div>
       <div className={`${classes.control} ${formInputsValid.email ? "" : classes.invalid}`}>
         <label>Email:</label>
@@ -83,11 +90,14 @@ const RegistrationComponent = () => {
           ref={emailInputRef}
           onChange={registrationLogic.changeHandler}
         ></input>
+        {!formInputsValid.email && <p>Unesite email</p>} 
       </div>
       <div>
-        <button className={classes.submit}>klik</button>
+        <button className={classes.submit}>Registruj se</button>
       </div>
     </form>
+    <ToastContainer />
+    </>
   );
 };
 
