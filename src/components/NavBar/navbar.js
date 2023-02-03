87c839a72c5caedbd38fe7d86b03b007a6bc2dae
Mladen <mladen.cvetanovic@outlook.com>
2023-02-03 16:27:50 +0100

    navbar responsive

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './main.css';
 import { Link } from 'react-router-dom';
 
@@ -7,20 +7,28 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
 
-  
+  const [isMobile, setIsMobile] = useState(false);
   
 
     return (
       <nav className="navbar">
         <Link to='/pocetna' className='logo'><i className="fa fa-bus"></i></Link>
         
-        <ul className='nav-menu'>
-          <li><Link to='/pocetna'><i className='fa-solid fa-house-user nav-links'>Pocetna</i></Link></li>
+        <ul className={isMobile ? "nav-links-mobile" : "nav-menu"}
+        onClick={() => setIsMobile(false)}>
+          <li><Link to='/pocetna'><i className='fa-solid fa-house-user nav-links'></i>Pocetna</Link></li>
           <li><Link to='/informacije'><i className='fa-solid fa-circle-info nav-links'></i>Informacije</Link></li>
           <li><Link to='/rezervacijakarte'><i className='fa fa-ticket nav-links'></i>Rezervacija Karte</Link></li>
           <li><Link to='/kontakt'><i className='fa-solid fa-address-book nav-links'></i>Kontakt</Link></li>
           <li><Link to='/login.component'><i className='fa fa-user-circle nav-links'></i>Prijavi se</Link></li>
           </ul>
+          <button className='mobile-menu-icon'
+          onClick={() => setIsMobile(!isMobile)}>
+            {isMobile ? (<i className='fas fa-times'></i> ) 
+            : (
+              <i className='fas fa-bars'></i>
+            ) }
+          </button>
       </nav>
 
      
--- src/components/registration/registration.logic.js
+++ src/components/registration/registration.logic.js
@@ -1,4 +1,4 @@
-import { useState, useRef } from "react";
+import { useState} from "react";
 import { toast } from "react-toastify";
 import RegistracijaApi from "../../api/registration.api";
 import COHelpers from "../../helpers/checkoutHelpers";
