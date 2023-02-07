c32993d8e6994dea23cedb425bd603670d7d4284
Lazar\Lazar <laki098@gmail.com>
2023-02-07 09:27:01 +0100

    iscitavanje linija iz baze za red voznje na pocetnoj (uslov za mesto polaska, mesto dolaska i datum)

--- backend/app/blueprints/linije_blueprint.py
+++ backend/app/blueprints/linije_blueprint.py
@@ -17,19 +17,21 @@ def linijeNov():
     vremePolaska = data["vremePolaska"]
     vremeDolaska = data["vremeDolaska"]
     prevoznik = data["prevoznik"]
+    datumPolaska = data["datumPolaska"]
+    datumDolaska = data["datumDolaska"]
 
-    form_linija = [mestoPolaska, mestoDolaska, vremePolaska, vremeDolaska, prevoznik]
+    form_linija = [mestoPolaska, mestoDolaska, vremePolaska, vremeDolaska, prevoznik, datumPolaska, datumDolaska]
 
 
     photoUrl = upload_file()
 
     if photoUrl == False:
-        return render_template(file_error="Invalid type of file!", mestoPolaska=form_linija[0], mestoDolaska=form_linija[1], vremePolaska=form_linija[2], vremeDolaska=form_linija[3], prevoznik=form_linija[4])
+        return render_template(file_error="Invalid type of file!", mestoPolaska=form_linija[0], mestoDolaska=form_linija[1], vremePolaska=form_linija[2], vremeDolaska=form_linija[3], prevoznik=form_linija[4], datumPolaska=form_linija[5], datumDolaska=form_linija[6])
 
 
     cursor = mydb.cursor(prepared=True)
-    q = "INSERT INTO linija VALUES(null,%s, %s, %s, %s, %s)"
-    all_keys = ["mestoPolaska", "mestoDolaska", "vremePolaska", "vremeDolaska", "prevoznik"]
+    q = "INSERT INTO linija VALUES(null,%s, %s, %s, %s, %s, %s, %s)"
+    all_keys = ["mestoPolaska", "mestoDolaska", "vremePolaska", "vremeDolaska", "prevoznik", "datumPolaska" , "datumDolaska"]
     data = default_values(data, all_keys)
     parameters = (
 
@@ -37,7 +39,9 @@ def linijeNov():
         data["mestoDolaska"],
         data["vremePolaska"],
         data["vremeDolaska"],
-        data["prevoznik"]
+        data["prevoznik"],
+        data["datumPolaska"],
+        data["datumDolaska"]
     )
     cursor.execute(q, parameters)
     mydb.commit()
@@ -50,16 +54,59 @@ def linije():
     cursor = mydb.cursor(prepared=True)
     cursor.execute("SELECT * FROM bus.linije")
     row = cursor.fetchall()
+    def toHrs(seconds):
+        seconds = seconds % (24 * 3600)
+        hour = seconds // 3600
+        seconds %= 3600
+        minutes = seconds // 60
+        seconds %= 60
+        return "%d:%02d:%02d" % (hour, minutes, seconds)
     list_linije = []
     for linija in row:
         list_linije.append({
             'id': linija[0],
             'mestoPolaska': linija[1],
             'mestoDolaska': linija[2],
-            'vremePolaska': linija[3],
-            'vremeDolaska': linija[4],
-            'prevoznik': linija[5]
+            'vremePolaska': toHrs(linija[3].seconds),
+            'vremeDolaska': toHrs(linija[4].seconds),
+            'prevoznik': linija[5],
+            'datumPolaska': str(linija[6].year) + '-' + str(linija[6].month) + '-' + str(linija[6].day),
+            'datumDolaska': str(linija[7].year) + '-' + str(linija[7].month) + '- ' + str(linija[7].day)
         })
 
   
+    return jsonify(list_linije)
+
+@linije_services.route("/filterLinija", methods=["POST"])
+def filterLinije():
+    a = request
+    mestoPolaska = request.json['mestoPolaska']
+    mestoDolaska = request.json['mestoDolaska']
+    datumPolaska = request.json['datumPolaska']
+    mydb = DB.connect()
+    cursor = mydb.cursor(prepared=True)
+    cursor.execute("SELECT * FROM bus.linije WHERE mestoPolaska=%s AND mestoDolaska=%s AND datumPolaska=%s ",(mestoPolaska, mestoDolaska, datumPolaska))
+    row = cursor.fetchall()
+    def toHrs(seconds):
+        seconds = seconds % (24 * 3600)
+        hour = seconds // 3600
+        seconds %= 3600
+        minutes = seconds // 60
+        seconds %= 60
+        return "%d:%02d:%02d" % (hour, minutes, seconds)
+    
+    list_linije = []
+    for linija in row:
+        list_linije.append({
+            'id': linija[0],
+            'mestoPolaska': linija[1],
+            'mestoDolaska': linija[2],
+            'vremePolaska': toHrs(linija[3].seconds),
+            'vremeDolaska': toHrs(linija[4].seconds),
+            'prevoznik': linija[5],
+            'datumPolaska': str(linija[6].year) + ', ' + str(linija[6].month) + ', ' + str(linija[6].day),
+            'datumDolaska': str(linija[7].year) + ', ' + str(linija[7].month) + ', ' + str(linija[7].day)
+        })
+
+
     return jsonify(list_linije)
--- src/components/NavBar/links/Pocetna.js
+++ src/components/NavBar/links/Pocetna.js
@@ -1,15 +1,25 @@
 import React, { useState, useEffect } from 'react'
+import LinijeApi from '../../../api/linije.api';
 import './pocetna.css';
 
 const Pocetna = () => {
+  const [filteredLinije, setFilteredLinije] = useState([])
+  const [valueDate, setValueDate] = useState('');
   const [val1, setVal1] = useState('')
   const [val2, setVal2] = useState('')
   const [linije, setLinije] = useState([])
 
+  const filterLinija = async() => {
+    if (!valueDate) return
+   
+    const response = await LinijeApi().filterLinija(val1,val2, valueDate)
+    const data = await response.json()
+    setFilteredLinije(data);
+  }
+
   const getLinije = async () => {
     const response = await fetch("http://localhost:5000/linije/linija");
     const data = await response.json();
-
     setLinije(data)
     setVal1(data[0].mestoPolaska)
     setVal2(data[0].mestoDolaska)
@@ -27,18 +37,36 @@ const Pocetna = () => {
 
   return (
     <div>
-        <select className='input' value={val1} onChange={(e) => {setVal1(e.target.value)}}>
+        <select className='input' value={val1} onChange={(e) => setVal1(e.target.value)}>
           {linije.map(linija => {
             return <option key={linija.id} value={linija.mestoPolaska}>{linija.mestoPolaska}</option>
+            
           })} 
         </select>
         <button className='fa-solid fa-repeat buttonSwitch  ' onClick={click}></button>
-        <select className='input' value={val2} onChange={(e) => {setVal2(e.target.value)}}>
+        <select className='input' value={val2} onChange={(e) => setVal2(e.target.value)}>
           {linije.map(linija => {
             return <option key={linija.id} value={linija.mestoDolaska}>{linija.mestoDolaska}</option>
           })}
+        
+        
           
         </select>
+        <label >Datum polaska</label>
+        <input type='date' value={valueDate} onChange= {(e) => setValueDate(e.target.value)} />
+        <button onClick={filterLinija}>Red voznje</button>
+
+        <div>
+          <ul>
+          {filteredLinije.map(linija => {
+            return <li key={linija.id}>
+              vreme dolaska: {linija.datumDolaska},
+              vreme polaska: {linija.datumPolaska},
+              prevoznik: {linija.prevoznik}
+            </li>
+          })}
+          </ul>
+        </div>
     </div>
   )
 }
