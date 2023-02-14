60e5ad0513f959158f93944440cb368baeaa1aa0
Mladen <mladen.cvetanovic@outlook.com>
2023-02-14 13:18:57 +0100

    no message

--- src/components/admin/admin.initial.js
+++ src/components/admin/admin.initial.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import LinijeApi from "../../api/linije.api";
 import AdminLogic from "./admin.logic";
+import { Link } from 'react-router-dom';
 
 const AdminInitial = () => {
   const [filteredLinije, setFilteredLinije] = useState([]);
@@ -78,7 +79,7 @@ const AdminInitial = () => {
               prevoznik: {linija.prevoznik},
               mesto polaska: {linija.mestoPolaska},
               mesto dolaska: {linija.mestoDolaska}
-              <button>zameni</button>
+              <button><Link to = '/admin.change.line'>zameni</Link></button>
               <button onClick={() => adminLogic.brisanjeLinije(linija.id)}>obrisi</button>
               
               
