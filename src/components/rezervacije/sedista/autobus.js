6ee8f2110fda6c09a7fa5d9b0e6989176023b67a
Makii94 <mladen.cvetanovic@skysoft.rs>
2023-08-10 14:57:28 +0200

    kondukter odabir sedista

--- src/components/rezervacije/sedista/autobus.js
+++ src/components/rezervacije/sedista/autobus.js
@@ -14,15 +14,20 @@ function Autobus() {
   ];
 
   const [rezervacije, setRezervacije] = useState(Array(55).fill(false));
-  const [trenutnaRezervacija, setTrenutnaRezervacija] = useState(null);
-  const [odabraniAutobus, setOdabraniAutobus] = useState(0);
+  const [trenutnaRezervacija, setTrenutnaRezervacija] = useState([]);
+  const [odabraniAutobus, setOdabraniAutobus] = useState(undefined);
 
   function handleClick(index) {
     const noviNiz = [...rezervacije];
     noviNiz[index] = !noviNiz[index];
 
+    if (noviNiz[index]) {
+      setTrenutnaRezervacija([...trenutnaRezervacija, index + 1]);
+    } else {
+      setTrenutnaRezervacija(trenutnaRezervacija.filter(s => s !== index + 1));
+    }
+
     setRezervacije(noviNiz);
-    setTrenutnaRezervacija(noviNiz[index] ? index + 1 : null);
   }
 
   function handleSelect(event) {
@@ -31,34 +36,42 @@ function Autobus() {
     const brojSedista = autobusi[index].brojSedista;
     const noviNiz = Array(brojSedista).fill(false);
     setRezervacije(noviNiz);
-    setTrenutnaRezervacija(null);
+    setTrenutnaRezervacija([]);
+  }
+
+  function handlePotvrdi() {
+    console.log('Izabrana sedišta:', trenutnaRezervacija);
+    // Ovde dodajte svoju logiku za potvrdu ili slanje podataka
   }
 
   return (
     <div>
-        <select value={odabraniAutobus} onChange={handleSelect}>
-          {autobusi.map((autobus, index) => (
-            <option key={index} value={index}>
-              {autobus.oznaka} - {autobus.brojSedista} sedišta
-            </option>
+      <select value={odabraniAutobus !== null ? odabraniAutobus : ""} onChange={handleSelect}>
+        <option value="">Izaberi autobus</option>
+        {autobusi.map((autobus, index) => (
+          <option key={index} value={index}>
+            {autobus.oznaka} - {autobus.brojSedista} sedišta
+          </option>
+        ))}
+      </select>
+
+      {odabraniAutobus !== undefined && (
+        <div className="autobus">
+          {rezervacije.map((rezervisano, index) => (
+            <div
+              key={index}
+              className={`sediste ${rezervisano ? 'rezervisano' : ''}`}
+              onClick={() => handleClick(index)}
+            >
+              {index + 1}
+            </div>
           ))}
-        </select>
-      
-    <div className="autobus">
-      
-      {rezervacije.map((rezervisano, index) => (
-        <div
-          key={index}
-          className={`sediste ${rezervisano ? 'rezervisano' : ''}`}
-          onClick={() => handleClick(index)}
-        >
-          {index + 1}
+          <div>Trenutno rezervisana mesta: {trenutnaRezervacija.length > 0 ? trenutnaRezervacija.join(', ') : 'Nijedno'}</div>
+          <button onClick={handlePotvrdi} disabled={trenutnaRezervacija.length === 0}>Potvrdi izbor</button>
         </div>
-      ))}
-      <div>Trenutno rezervisano mesto: {trenutnaRezervacija || 'Nijedno'}</div>
-    </div>
+      )}
     </div>
   );
 }
 
-export default Autobus; 
+export default Autobus;
