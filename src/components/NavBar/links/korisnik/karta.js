4002051f9dc71c7ae81443a13f9bf5dee2e5f736
Lazar\Lazar <laki098@gmail.com>
2023-09-26 21:30:05 +0200

    Merge branch 'lazarfix' of https://github.com/laki098/bus-kart into lazarfix
    # Conflicts:
    #	backend/qrcode.png
    #	src/components/NavBar/links/korisnik/karta.js

--- src/components/NavBar/links/korisnik/karta.js
+++ src/components/NavBar/links/korisnik/karta.js
@@ -27,10 +27,19 @@ const Karta = () => {
     });
     const data = await response.json();
 
-    // Razdvojite karte na neiskoriscene i iskoriscene
-    const sveKarte = data.karte;
-    const neiskorisceneKarte = [];
-    const iskorisceneKarte = [];
+    const a1 = data.karte.map((item) => {
+      console.log(item.polaznaStanicaR);
+      return {
+        brojMesta: item.brojMesta,
+        pocetna: item.polaznaStanicaR,
+        krajnja: item.krajnjaStanicaR,
+        datumP: item.datumPolaska,
+        datumD: item.datumDolaska,
+        vremeP: item.vremePolaska,
+        vremeD: item.vremeDolaska,
+        cekiranje: item.cekiran,
+      };
+    });
 
     sveKarte.forEach((karta) => {
       if (karta.iskoriscena) {
@@ -53,70 +62,74 @@ const Karta = () => {
     getKarte();
   }, []);
 
-  useEffect(() => {
-    // Pratite promene u neiskorisceneKarte i ažurirajte iskorisceneKarte
-    const updatedIskorisceneKarte = neiskorisceneKarte.filter(
-      (karta) => karta.cekiran
-    );
-    setIskorisceneKarte(updatedIskorisceneKarte);
-  }, [neiskorisceneKarte]);
-
   return (
     <>
       <div>
-        <h2>Neiskoriscene karte:</h2>
-        <ul>
-          {neiskorisceneKarte.map((karte) => (
-            <li className="lista-stavka" key={karte.brojMesta}>
-              <p className="naslov">Broj rezervisanih mesta</p>
-              <div className="vrednost">{karte.brojMesta}</div>
-              <p className="naslov">Cekirana karta</p>
-              <div className="vrednost">
-                {karte.cekiran ? "Da" : karte.cekiran === false ? "Ne" : "N/A"}
-              </div>
-              <p className="naslov">Polazna stanica</p>
-              <div className="vrednost">{karte.polaznaStanicaR}</div>
-              <p className="naslov">Dolazna stanica</p>
-              <div className="vrednost">{karte.krajnjaStanicaR}</div>
-              <p className="naslov">Datum Polaska</p>
-              <div className="vrednost">{karte.datumPolaska}</div>
-              <p className="naslov">Datum dolaska</p>
-              <div className="vrednost">{karte.datumDolaska}</div>
-              <p className="naslov">Vreme Polaska</p>
-              <div className="vrednost">{karte.vremePolaska}</div>
-              <p className="naslov">Vreme dolaska</p>
-              <div className="vrednost">{karte.vremeDolaska}</div>
-            </li>
-          ))}
-        </ul>
-      </div>
-
-      <div>
-        <h2>Iskoriscene karte:</h2>
-        <ul>
-          {iskorisceneKarte.map((karte) => (
-            <li className="lista-stavka" key={karte.brojMesta}>
-              <p className="naslov">Broj rezervisanih mesta</p>
-              <div className="vrednost">{karte.brojMesta}</div>
-              <p className="naslov">Cekirana karta</p>
-              <div className="vrednost">
-                {karte.cekiran ? "Da" : karte.cekiran === false ? "Ne" : "N/A"}
-              </div>
-              <p className="naslov">Polazna stanica</p>
-              <div className="vrednost">{karte.polaznaStanicaR}</div>
-              <p className="naslov">Dolazna stanica</p>
-              <div className="vrednost">{karte.krajnjaStanicaR}</div>
-              <p className="naslov">Datum Polaska</p>
-              <div className="vrednost">{karte.datumPolaska}</div>
-              <p className="naslov">Datum dolaska</p>
-              <div className="vrednost">{karte.datumDolaska}</div>
-              <p className="naslov">Vreme Polaska</p>
-              <div className="vrednost">{karte.vremePolaska}</div>
-              <p className="naslov">Vreme dolaska</p>
-              <div className="vrednost">{karte.vremeDolaska}</div>
-            </li>
-          ))}
-        </ul>
+        <div>Moje karte</div>
+        <div>
+          <div>
+            <h2>Aktivne karte</h2>
+            <div className="Grupa">
+              {sveKarte
+                .filter((karte) => karte.cekiranje === false)
+                .map((karte) => (
+                  <div key={karte.id}>
+                    <li className="lista-stavka">
+                      <p className="naslov">Broj rezervisanih mesta</p>
+                      <div className="vrednost">{karte.brojMesta}</div>
+                      <p className="naslov">Polazna stanica</p>
+                      <div className="vrednost">{karte.pocetna}</div>
+                      <p className="naslov">Dolazna stanica</p>
+                      <div className="vrednost">{karte.krajnja}</div>
+                      <p className="naslov">Datum Polaska</p>
+                      <div className="vrednost">{karte.datumP}</div>
+                      <p className="naslov">Datum dolaska</p>
+                      <div className="vrednost">{karte.datumD}</div>
+                      <p className="naslov">Vreme Polaska</p>
+                      <div className="vrednost">{karte.vremeP}</div>
+                      <p className="naslov">Vreme dolaska</p>
+                      <div className="vrednost">{karte.vremeD}</div>
+                      <p className="naslov">Cekiran</p>
+                      <div className="vrednost">
+                        {karte.cekiranje === false ? <p>NE</p> : <>da</>}
+                      </div>
+                    </li>
+                  </div>
+                ))}
+            </div>
+          </div>
+          <div>
+            <h2>Neaktivne karte</h2>
+            <div className="Grupa">
+              {sveKarte
+                .filter((karte) => karte.cekiranje === true)
+                .map((karte) => (
+                  <div key={karte.id}>
+                    <li className="lista-stavka">
+                      <p className="naslov">Broj rezervisanih mesta</p>
+                      <div className="vrednost">{karte.brojMesta}</div>
+                      <p className="naslov">Polazna stanica</p>
+                      <div className="vrednost">{karte.pocetna}</div>
+                      <p className="naslov">Dolazna stanica</p>
+                      <div className="vrednost">{karte.krajnja}</div>
+                      <p className="naslov">Datum Polaska</p>
+                      <div className="vrednost">{karte.datumP}</div>
+                      <p className="naslov">Datum dolaska</p>
+                      <div className="vrednost">{karte.datumD}</div>
+                      <p className="naslov">Vreme Polaska</p>
+                      <div className="vrednost">{karte.vremeP}</div>
+                      <p className="naslov">Vreme dolaska</p>
+                      <div className="vrednost">{karte.vremeD}</div>
+                      <p className="naslov">Cekiran</p>
+                      <div className="vrednost">
+                        {karte.cekiranje === false ? <p>NE</p> : <>DA</>}
+                      </div>
+                    </li>
+                  </div>
+                ))}
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
