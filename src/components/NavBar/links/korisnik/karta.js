65edc23877c1167c380c3e5387020800858dc3fe
Makii94 <mladen.cvetanovic@skysoft.rs>
2023-09-26 16:00:01 +0200

    karta

--- src/components/NavBar/links/korisnik/karta.js
+++ src/components/NavBar/links/korisnik/karta.js
@@ -1,10 +1,13 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import cookies from "js-cookie";
 import "./still.css";
 
 const Karta = () => {
   const [sveKarte, setSveKarte] = useState([]);
-  //? izvlacenje korisnika koji je prijavljen
+  const [neiskorisceneKarte, setNeiskorisceneKarte] = useState([]);
+  const [iskorisceneKarte, setIskorisceneKarte] = useState([]);
+  
+  // Izvlačenje korisnika koji je prijavljen
   let userData = cookies.get("userData");
   let userPars = {};
   if (userData != undefined) {
@@ -16,58 +19,100 @@ const Karta = () => {
 
   const getKarte = async (userId) => {
     const response = await fetch("http://localhost:5000/korisnik/karta", {
-      method: "POST", // Postavite HTTP metod na POST
+      method: "POST",
       headers: {
-        "Content-Type": "application/json", // Postavite Content-Type na JSON
+        "Content-Type": "application/json",
       },
-      body: userIdP, // Postavite telo zahteva na JSON podatke
+      body: userIdP,
     });
     const data = await response.json();
 
-    const a1 = data.karte.map((item) => {
-      console.log(item.polaznaStanicaR);
-      return {
-        brojMesta: item.brojMesta,
-        pocetna: item.polaznaStanicaR,
-        krajnja: item.krajnjaStanicaR,
-        datumP: item.datumPolaska,
-        datumD: item.datumDolaska,
-        vremeP: item.vremePolaska,
-        vremeD: item.vremeDolaska,
-      };
-    });
+    // Razdvojite karte na neiskoriscene i iskoriscene 
+    const sveKarte = data.karte;
+    const neiskorisceneKarte = [];
+    const iskorisceneKarte = [];
 
-    setSveKarte(a1);
+    sveKarte.forEach((karta) => {
+      if (karta.iskoriscena) {
+        iskorisceneKarte.push(karta); 
+      } else {
+        neiskorisceneKarte.push(karta); 
+      }
+    });
+    console.log(iskorisceneKarte)
+
+    setSveKarte(sveKarte);
+    setNeiskorisceneKarte(neiskorisceneKarte); // Postavite neiskoriscene karte
+    setIskorisceneKarte(iskorisceneKarte); // Postavite iskoriscene karte
   };
 
-  console.log(sveKarte);
+  console.log( neiskorisceneKarte);
+  console.log( iskorisceneKarte);
+
   useEffect(() => {
     getKarte();
   }, []);
+
+  useEffect(() => {
+    // Pratite promene u neiskorisceneKarte i ažurirajte iskorisceneKarte
+    const updatedIskorisceneKarte = neiskorisceneKarte.filter(
+      (karta) => karta.cekiran
+    );
+    setIskorisceneKarte(updatedIskorisceneKarte);
+  }, [neiskorisceneKarte]);
+
   return (
     <>
-      <div>Moje karte</div>
       <div>
-        {sveKarte.map((karte) => {
-          return (
-            <li className="lista-stavka">
+        <h2>Neiskoriscene karte:</h2>
+        <ul>
+          {neiskorisceneKarte.map((karte) => (
+            <li className="lista-stavka" key={karte.brojMesta}>
               <p className="naslov">Broj rezervisanih mesta</p>
               <div className="vrednost">{karte.brojMesta}</div>
+              <p className="naslov">Cekirana karta</p>
+              <div className="vrednost">{karte.cekiran ? 'Da' : karte.cekiran === false ? 'Ne' : 'N/A'}</div>
               <p className="naslov">Polazna stanica</p>
-              <div className="vrednost">{karte.pocetna}</div>
+              <div className="vrednost">{karte.polaznaStanicaR}</div>
               <p className="naslov">Dolazna stanica</p>
-              <div className="vrednost">{karte.krajnja}</div>
+              <div className="vrednost">{karte.krajnjaStanicaR}</div>
               <p className="naslov">Datum Polaska</p>
-              <div className="vrednost">{karte.datumP}</div>
+              <div className="vrednost">{karte.datumPolaska}</div>
               <p className="naslov">Datum dolaska</p>
-              <div className="vrednost">{karte.datumD}</div>
+              <div className="vrednost">{karte.datumDolaska}</div>
               <p className="naslov">Vreme Polaska</p>
-              <div className="vrednost">{karte.vremeP}</div>
+              <div className="vrednost">{karte.vremePolaska}</div>
               <p className="naslov">Vreme dolaska</p>
-              <div className="vrednost">{karte.vremeD}</div>
+              <div className="vrednost">{karte.vremeDolaska}</div>
             </li>
-          );
-        })}
+          ))}
+        </ul>
+      </div>
+
+      <div>
+        <h2>Iskoriscene karte:</h2>
+        <ul>
+          {iskorisceneKarte.map((karte) => (
+            <li className="lista-stavka" key={karte.brojMesta}>
+              <p className="naslov">Broj rezervisanih mesta</p>
+              <div className="vrednost">{karte.brojMesta}</div>
+              <p className="naslov">Cekirana karta</p>
+              <div className="vrednost">{karte.cekiran ? 'Da' : karte.cekiran === false ? 'Ne' : 'N/A'}</div>
+              <p className="naslov">Polazna stanica</p>
+              <div className="vrednost">{karte.polaznaStanicaR}</div>
+              <p className="naslov">Dolazna stanica</p>
+              <div className="vrednost">{karte.krajnjaStanicaR}</div>
+              <p className="naslov">Datum Polaska</p>
+              <div className="vrednost">{karte.datumPolaska}</div>
+              <p className="naslov">Datum dolaska</p>
+              <div className="vrednost">{karte.datumDolaska}</div>
+              <p className="naslov">Vreme Polaska</p>
+              <div className="vrednost">{karte.vremePolaska}</div>
+              <p className="naslov">Vreme dolaska</p>
+              <div className="vrednost">{karte.vremeDolaska}</div>
+            </li>
+          ))}
+        </ul>
       </div>
     </>
   );
