a7b277c7963b92fbda8dd79f47bbdabf533fa78c
Lazar\Lazar <laki098@gmail.com>
2023-10-26 17:23:37 +0200

    cena

---
+++ backend/Routes/CenaRoute.js
@@ -0,0 +1,18 @@
+import express from "express";
+import Cena from "../Models/Cena.js";
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  try {
+    const cena = await Cena.findAll({
+      attributes: {
+        exclude: ["createdAt", "updatedAt"],
+      },
+    });
+    return res.json({ cena });
+  } catch (error) {
+    res.status(500).json({ message: "eeaeasrasr", error });
+  }
+});
+
+export default router;
