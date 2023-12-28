59bbec0c2c24bd5280e532c66fda1787760f24c7
SlobodankaN <slobodankanedeljkovic8@gmail.com>
2023-12-28 15:17:21 +0100

    Sitne dorade u CSS u odnosu na moje podesavanje na Desktop-u iz perioda novembar 2023

--- src/components/admin/bus.initional.js
+++ src/components/admin/bus.initional.js
@@ -90,7 +90,7 @@ const BusInitional = () => {
                         {/*  column centar   */}
                         <Trans i18nKey="description.part170">Oznaka </Trans>
                       </div>
-                      <div className="kolona-bus podaci ">
+                      <div className="kolona-bus podaci sirina-info-4">
                         {" "}
                         {bus.oznakaBusa}
                       </div>{" "}
